e050914195e680bb320ab5ed75a9c6f02e051890
Eugen Bejenaru <48156218+EugenBj@users.noreply.github.com>
2020-08-13 16:42:43 +0100

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+        
+    return <div className="note">
+        <h1>This is the blog title</h1>
+        <p>This is the blog content</p>
+    </div>
+}
+
+export default Note;
